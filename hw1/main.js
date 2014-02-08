48c7166ebbb68739481a8745a04b4004fb510b23
Keroles <keroles.anwar@gmail.com>
2014-02-08 10:52:45 -0500

    Added checkMobile function

--- hw1/main.js
+++ hw1/main.js
@@ -1,4 +1,20 @@
 $(w).resize(function(){ //Update dimensions on resize
 	sw = document.body.clientWidth;
 	sh = document.body.clientHeight;
+	checkMobile();
 });
+
+//Check if Mobile
+function checkMobile() {
+  mobile = (sw > breakpoint) ? false : true;
+
+    if (!mobile) { //If Not Mobile
+        loadAux(); 
+	$('.aux header a').addClass('disabled').addClass('open');
+	$('[role="tabpanel"],#nav,#search').show(); //Show full navigation and search
+     } else { //Hide 
+              if(!$('#nav-anchors a').hasClass('active')) {
+                    $('#nav,#search').hide(); //Hide full navigation and search
+               }
+     }
+}
